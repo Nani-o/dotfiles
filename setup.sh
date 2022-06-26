afd293da9de2bc3417a568f7e22e2ff247d1fd97
Sofiane Medjkoune <sofiane@medjkoune.fr>
2022-06-26 21:51:27 +0200

    Add theme install

--- setup.sh
+++ setup.sh
@@ -2,6 +2,15 @@
 
 [[ ! -d "${HOME}/.oh-my-zsh" ]] && RUNZSH=no sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+REPO_PATH=~/.oh-my-zsh/custom/themes/powerlevel10k
+REPO_URL="https://github.com/romkatv/powerlevel10k"
+
+[[ -d "$REPO_PATH" ]] \
+  && git -C "$REPO_PATH" pull \
+  || git clone "$REPO_URL" "$REPO_PATH"
+
+[[ ! -d "${HOME}/.oh-my-zsh" ]] && RUNZSH=no sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
 REPO_PATH=~/.dotfiles
 REPO_URL="https://github.com/Nani-o/dotfiles"
 
