d851e5056155f894a18d5b3cca195ac435fd8e91
Sofiane Medjkoune <sofiane@medjkoune.fr>
2023-05-08 17:49:13 +0200

    Update symlinks script

--- symlinks.sh
+++ symlinks.sh
@@ -1,26 +1,45 @@
 #!/bin/zsh -e
 
+# Repo path
 REPO_PATH=~/.dotfiles
 
-find "$REPO_PATH" -maxdepth 1 -type f \( -iname '.*' ! -iname '.travis.yml' ! -iname '.gitignore' \) \
-    | xargs -I {} ln -fs "{}" "${HOME}/"
+# Temp file to store symlinks created
+TMPFILE=$(mktemp -t symlinks)
+trap 'rm -f "$TMPFILE"' EXIT
 
-NESTED=$(find "$REPO_PATH" -mindepth 2 -type f \( -iname '*' -ipath ${REPO_PATH}'/.*/*' ! -ipath '*.git/*' \))
+# Where the symlinks list is stored
+EXISTING_SYMLINKS="${REPO_PATH}/.symlinks"
+touch "${EXISTING_SYMLINKS}"
 
-while read -r broken_link
+# List of files to symlink and folders to create
+ROOT_FILES=$(find "$REPO_PATH" -maxdepth 1 -type f \( -iname '.*' ! -iname '.travis.yml' ! -iname '.gitignore' \))
+NESTED_FILES=$(find "$REPO_PATH" -mindepth 2 -type f \( -iname '*' -ipath ${REPO_PATH}'/.*/*' ! -ipath '*.git/*' \))
+NESTED_FOLDERS=$(echo "${NESTED_FILES}" | xargs dirname | sort | uniq)
+
+# Create root level symlinks
+while read -r FILE
 do
-    [[ -L "$broken_link" ]] || continue
-    [[ -e "$broken_link" ]] && continue
-    target=$(readlink "$broken_link")
-    [[ "$target" == "${HOME}/.dotfiles/"* ]] && unlink "$broken_link"
-done <<< "$(find "${HOME}" -maxdepth 3 -type l ! -exec test -e {} \; -print)"
+    RELATIVE_PATH=$(echo "${FILE}" | sed "s@${REPO_PATH}/@@g")
+    ln -fs "${REPO_PATH}/${RELATIVE_PATH}" "${HOME}/${RELATIVE_PATH}"
+    echo "${HOME}/${RELATIVE_PATH}" >> "${TMPFILE}"
+done <<< "${ROOT_FILES}"
 
-echo "${NESTED}" \
-    | xargs dirname \
-    | sed "s@$REPO_PATH/@${HOME}/@g" \
-    | xargs -I {} mkdir -p "{}"
+# Create nested folders
+while read -r FOLDER
+do
+    mkdir -p "${HOME}/${FOLDER}"
+done <<< "${NESTED_FOLDERS}"
 
-echo "${NESTED}" \
-    | sed "s@$REPO_PATH/@@g" \
-    | xargs -I {} ln -fs "${REPO_PATH}/{}" "${HOME}/{}"
+# Create nested symlinks
+while read -r FILE
+do
+    RELATIVE_PATH=$(echo "${FILE}" | sed "s@${REPO_PATH}/@@g")
+    ln -fs "${REPO_PATH}/${RELATIVE_PATH}" "${HOME}/${RELATIVE_PATH}"
+    echo "${HOME}/${RELATIVE_PATH}" >> "${TMPFILE}"
+done <<< "${NESTED_FILES}"
 
+# Remove symlinks that are no longer in the repo
+diff "${TMPFILE}" "${EXISTING_SYMLINKS}" | grep '>' | sed 's/^> //g' | xargs unlink 
+
+# Update the list of symlinks installed
+cat "${TMPFILE}" > "${EXISTING_SYMLINKS}"
