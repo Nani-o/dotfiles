54cdab7ff726a649d4cd7f8f42492155c8879fc0
Sofiane Medjkoune <sofiane@medjkoune.fr>
2023-08-21 13:41:04 +0200

    Not sure

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ which wget > /dev/null
 if [[ "$?" == 0 ]]; then
   [[ ! -d "${HOME}/.oh-my-zsh" ]] && RUNZSH=no sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 else
+  echo test
   [[ ! -d "${HOME}/.oh-my-zsh" ]] && RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 REPO_PATH=~/.oh-my-zsh/custom/themes/powerlevel10k
 REPO_URL="https://github.com/romkatv/powerlevel10k"
 
-[[ -d "$REPO_PATH" ]] \
+[[ ! -d "$REPO_PATH" ]] \
   || git clone "$REPO_URL" "$REPO_PATH"
 
 REPO_PATH=~/.dotfiles
