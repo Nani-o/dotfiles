a6c8680e48bb9921b6e41183a1cee8568589e080
Sofiane Medjkoune <sofiane@medjkoune.fr>
2023-10-10 15:15:24 +0200

    Allow for portrait usage and add function to add local sheets not synced

--- .funcs/sheets.sh
+++ .funcs/sheets.sh
@@ -6,6 +6,9 @@ function sheets {
   new)
     __sheets_new
     ;;
+  local_new)
+    __sheets_local_new
+    ;;
   "")
     __sheets
     ;;
@@ -32,6 +35,19 @@ function __sheets_new {
     fi
 }
 
+function __sheets_local_new {
+    echo -n "How do you want to name these local file : "
+    read FILE
+    if [[ -f ~/.cheatsheets/"$FILE".md ]]
+    then
+        echo "Already exists"
+    else
+        [[ "$FILE" == *.md ]] \
+          && "$EDITOR" ~/.cheatsheets/"${FILE}" \
+          || "$EDITOR" ~/.cheatsheets/"${FILE}".md
+    fi
+}
+
 function __sheets_help {
     echo -e "usage: sheets [subcommand]\n
 Theses are sheets subcommands:
@@ -45,14 +61,17 @@ function __file_from_title {
 
 function __preview_sheets_from_title {
     file=$(echo "$1" | sed 's@\ *|\ *@_@' | sed 's/\ /_/g')
-    width=$(echo $(($COLUMNS*0.7-5)) | cut -d "." -f 1)
+    [[ "$COLUMNS" -gt $(($LINES*2)) ]] && width=$(echo $(($COLUMNS*0.7-5)) | cut -d "." -f 1)
+    [[ "$COLUMNS" -le $((LINES*2)) ]] && width=$(echo $(($COLUMNS-5)) | cut -d "." -f 1)
     rich -w $width ~/.cheatsheets/"$file".md --force-terminal
 }
 
 function __sheets {
+    [[ "$COLUMNS" -gt $(($LINES*2)) ]] && preview_windows=right,70%
+    [[ "$COLUMNS" -le $((LINES*2)) ]] && preview_windows=down,90%
     sheets_list=$(fd . ~/.cheatsheets -e md | sed 's/.*.cheatsheets\///g' | sed 's@_@ | @' | column -t | sed 's/_/ /g' | sed 's/.md$//g')
-    sheet_file=$(echo "$sheets_list" | fzf --preview "source ~/.funcs/sheets.sh;__preview_sheets_from_title {}" \
-                              --preview-window right,70% --ansi --no-scrollbar)
+    sheet_file=$(echo "$sheets_list" | fzf --layout=reverse --preview "source ~/.funcs/sheets.sh;__preview_sheets_from_title {}" \
+                              --preview-window $preview_windows --ansi --no-scrollbar)
 
     if [[ ! -z "$sheet_file" ]]
     then
