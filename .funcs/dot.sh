ccdbf00914f002f0c4e2146201a8d8e997befb53
Sofiane MEDJKOUNE <sofiane@medjkoune.fr>
2026-07-12 18:22:36 +0200

    Migrate update and setup functions to be subcommand of the dot helper

--- .funcs/dot.sh
+++ .funcs/dot.sh
@@ -1,6 +1,12 @@
 function dot {
   case $1 in
-  update|pull)
+  setup)
+    __dot_setup_workstation
+    ;;
+  update)
+    __dot_update "$2"
+    ;;
+  pull)
     __dot_pull "$2"
     ;;
   status)
@@ -38,11 +44,14 @@ function dot {
 function __dot_help {
     echo -e "usage: dot [subcommand]\n
 Theses are dot subcommands:
-  update:   download the latest commits of dotfiles and reload zsh
+  setup:    setup this workstation
+  update:   update dotfiles, packages, and system software
+  pull:     download the latest commits of dotfiles and reload zsh
   status:   show the git status of dotfiles
   commit:   commit the local changes of dotfiles
   push:     push the local changes of dotfiles
   git:      run git command in dotfiles
+  link:     link all dotfiles in the home folder
   help:     show this help message
   reload:   reload zsh"
 }
@@ -60,6 +69,114 @@ function __dot_pull {
     ~/.dotfiles/symlinks.sh
     [[ "$1" != "--no-reload" ]] && __dot_reload
   fi
+}
+
+# Test if binary exists using zsh
+function __dot_exists {
+  if [[ -x "$(command -v "$1")" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function __dot_setup_workstation {
+  wsl_conf_custom_sha1="$(sha1sum $HOME/.dotfiles/other/wsl.conf 2>/dev/null | awk '{print $1}')"
+  wsl_conf_current_sha1="$(sha1sum /etc/wsl.conf 2>/dev/null | awk '{print $1}')"
+  if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop && $wsl_conf_custom_sha1 != $wsl_conf_current_sha1 ]]; then
+    echo "[bash] WSL detected"
+    read -q "REPLY?Do you want to install /etc/wsl.conf file ? [y/N]"
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      __dot_setup_wsl
+    fi
+  fi
+
+  # Install Homebrew
+  if ! __dot_exists brew; then
+    echo "[bash] Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    source ~/.zshrc
+  else
+    echo "[bash] Homebrew is already installed"
+  fi
+
+  echo "[brew] Installing Homebrew packages..."
+  brew bundle --file="$HOME/.dotfiles/Brewfile"
+  rm -f "$HOME/.dotfiles/Brewfile.lock.json"
+
+  __dot_install_ansible
+
+  pipx_packages=(
+    httpie
+  )
+
+  __dot_install_pipx_packages "${pipx_packages[@]}"
+}
+
+function __dot_install_ansible {
+  # Install Ansible
+  ansible_pipx_packages=(
+    ansible-core
+    ansible-lint
+  )
+  __dot_install_pipx_packages "${ansible_pipx_packages[@]}"
+  pipx inject ansible-core ansible
+}
+
+function __dot_setup_wsl {
+  echo "[bash] Installing /etc/wsl.conf file..."
+  sudo cp "$HOME/.dotfiles/other/wsl.conf" /etc/wsl.conf
+  read -q "REPLY?Do you want to stop WSL in order to changes to be applied ? [y/N]"
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    wsl.exe --shutdown
+  fi
+}
+
+function __dot_install_pipx_packages {
+  # Install pipx packages
+  pipx_packages=("$@")
+  for package in "${pipx_packages[@]}"; do
+    if pipx list | grep -q "$package"; then
+      echo "[pipx] $package is already installed"
+    else
+      echo "[pipx] Installing $package..."
+      pipx install "$package"
+    fi
+  done
+}
+
+function __dot_update {
+  separator "update dotfiles"
+  dot pull --no-reload
+  separator "omz update"
+  "$ZSH/tools/upgrade.sh"
+  if __dot_exists brew
+  then
+    separator "brew update"
+    brew update
+    separator "brew upgrade"
+    brew upgrade
+  fi
+  if __dot_exists pipx
+  then
+    separator "pipx upgrade-all"
+    pipx upgrade-all
+  fi
+  if [[ -f /etc/debian_version ]]
+  then
+    separator "apt update"
+    sudo apt -y update
+    separator "apt upgrade"
+    sudo apt -y upgrade
+    separator "apt autoremove"
+    sudo apt -y autoremove
+  fi
+  if [[ "$OSTYPE" == "darwin"* ]]
+  then
+    separator "softwareupdate -i -a"
+    sudo softwareupdate -i -a
+  fi
+  [[ "$1" != "--no-reload" ]] && dot reload
 }
 
 function __dot_commit {
